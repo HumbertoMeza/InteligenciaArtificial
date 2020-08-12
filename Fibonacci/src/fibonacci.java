4f0797b321010eeb7c2a9ef56a95ff582cc79ece
mancpato <manc@uabcs.mx>
2020-08-11 20:39:42 -0600

    Update fibonacci.java
    Cambios menores

--- Fibonacci/src/fibonacci.java
+++ Fibonacci/src/fibonacci.java
@@ -1,13 +1,14 @@
+/**
+	Humberto Meza
+*/
 
 public class fibonacci {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int Numerofib = 20;
 		int num1 = 0;
 		int num2 = 1;
 		int sumatoria = 0;
-		
 		
 		for(int j = 1; j<= Numerofib; j++) {
 			System.out.println(j +".- "+ num1);
