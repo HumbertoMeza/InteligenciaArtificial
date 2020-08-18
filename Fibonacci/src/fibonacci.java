2bd9b4c1d36d085fc826359e9ccf72e46f7acbc8
HumbertoMeza <56142422+HumbertoMeza@users.noreply.github.com>
2020-08-18 12:22:09 -0600

    Recursivo

--- Fibonacci/src/fibonacci.java
+++ Fibonacci/src/fibonacci.java
@@ -8,7 +8,7 @@ public class fibonacci {
 		System.out.println("Ingresa el numero de fibonacci que quieres");
 		
 		int numerofib = leer.nextInt();
-		int num1 = 0;
+		/*int num1 = 0;
 		int num2 = 1;
 		int sumatoria = 0;
 		
@@ -20,7 +20,18 @@ public class fibonacci {
 			num1 = num2;
 			num2 = sumatoria;
 		}
-		System.out.println("Numero fibonacci #"+ numerofib + " "+ num1);
+		System.out.println("Numero fibonacci #"+ numerofib + " "+ num1);*/
+		
+		System.out.println("Numero fibonacci: "+ recursiva(numerofib+1));
 	}
-
+	public static int recursiva(int n) {
+		if(n==1) {
+			return 0;
+		}
+		if(n==2) {
+			return 1;
+		}
+		return recursiva(n-1)+recursiva(n-2);
+		
+	}
 }
